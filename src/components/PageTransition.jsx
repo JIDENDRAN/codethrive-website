a244caf67cf48b55e554163073787bf8448fb695
Kishore kumar <kishorekumar3732@gmail.com>
2025-11-05 15:54:22 +0530

    Updated files with latest changes

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -7,6 +7,7 @@ export default function PageTransition({ children }) {
       animate={{ opacity: 1, scale: 1, x: 0 }}
       exit={{ opacity: 0, scale: 0.98, x: -60 }}
       transition={{
+        
         duration: 0.6,
         ease: [0.22, 1, 0.36, 1], // Smooth “easeOutExpo” curve
       }}
